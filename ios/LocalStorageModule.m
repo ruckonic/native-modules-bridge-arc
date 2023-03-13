43d8fb01723900a049188f0f5e2121fbf5bf0269
Rene Corrales <ruckonic@icloud.com>
2023-03-12 23:29:36 -0600

    feat(ios): constants modules

---
+++ ios/LocalStorageModule.m
@@ -0,0 +1,12 @@
+//
+//  LocalStorageModule.m
+//  RNLocalStorage
+//
+//  Created by Rene Corrales on 11/3/23.
+//
+
+#import "React/RCTBridgeModule.h"
+
+/// Define the module interface
+@interface RCT_EXTERN_MODULE(LocalStorageModule, NSObject)
+@end
