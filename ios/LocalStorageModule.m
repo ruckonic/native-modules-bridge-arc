ec4e58272a2de9ccbaf71a45cf5d5ecc31f9c843
Rene Corrales <ruckonic@icloud.com>
2023-03-17 23:10:23 -0600

    feat(ios): implement methods

--- ios/LocalStorageModule.m
+++ ios/LocalStorageModule.m
@@ -9,4 +9,10 @@
 
 /// Define the module interface
 @interface RCT_EXTERN_MODULE(LocalStorageModule, NSObject)
+
+RCT_EXTERN__BLOCKING_SYNCHRONOUS_METHOD(sayHello: (NSString *) name)
+
+RCT_EXTERN_METHOD(getTodos: (RCTPromiseResolveBlock) resolve rejecter:(RCTPromiseRejectBlock) reject)
+
+RCT_EXTERN_METHOD(getTodosCb: (RCTResponseSenderBlock) callback)
 @end
--- ios/LocalStorageModule.swift
+++ ios/LocalStorageModule.swift
@@ -6,6 +6,14 @@
 //
 
 import React
+import Foundation
+
+struct Todo: Codable {
+  let id: Int
+  let title: String
+  let userId: Int
+  let completed: Bool
+}
 
 @objc(LocalStorageModule)
 class LocalStorageModule: NSObject {
@@ -28,5 +36,63 @@ class LocalStorageModule: NSObject {
   /// Necessary to instance module without constructor
   @objc static func requiresMainQueueSetup() -> Bool {
     return true
+  }
+  
+  @objc func getTodos(_ resolve: RCTPromiseResolveBlock, rejecter reject: RCTPromiseRejectBlock) -> Void {
+    do {
+      let todos = try self.fetchTodos()
+      
+      let todosMap = self.todosToMap(todos)
+      
+      resolve(todosMap)
+    } catch {
+      reject("Internal", "Error obtain", error)
+    }
+  }
+  
+  private func todosToMap(_ todos: [Todo]) -> [[String: Any]] {
+    return todos.map { todo in
+      return  [
+          "id": todo.id,
+          "title": todo.title,
+          "userId": todo.userId,
+          "completed": todo.completed
+        ]
+      
+    }
+  }
+  
+  private func fetchTodos() throws -> [Todo] {
+    let url = URL(string: "https://jsonplaceholder.typicode.com/todos")
+    let semaphore = DispatchSemaphore(value: 0)
+    var result: Data = Data()
+    
+       
+    let task = URLSession.shared.dataTask(with: url!) {(data, response, error) in
+      result.append(data!)
+      semaphore.signal()
+    }
+       
+    task.resume()
+    semaphore.wait()
+    let jsonDecoder = JSONDecoder()
+    return try jsonDecoder.decode([Todo].self, from: result)
+      
+  }
+  
+  @objc func getTodosCb(_ callback: RCTResponseSenderBlock) {
+    do {
+      let todos = try self.fetchTodos()
+      
+      let todosMap = self.todosToMap(todos)
+      
+      callback([NSNull(), todosMap])
+    } catch {
+      callback([error.localizedDescription, NSNull()])
+    }
+  }
+  
+  @objc func sayHello(_ name: String) -> String {
+     return "hello \(name)"
   }
 }
